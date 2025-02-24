cd539cc2dcf40e832985707435ab9bb3eb381589
amitmahato98 <shankhakush@gmail.com>
2025-02-24 23:41:21 +0545

    drawer made frontend

--- lib/datatypes/datatypes.dart
+++ lib/datatypes/datatypes.dart
@@ -7,3 +7,4 @@ const Color brightWhite = Color(0xFFFFFFFF);
 const Color darkBlue = Color(0xFF167AFA);
 const Color darkGray = Color(0xFFA9A9A9);
 const Color darkBlack = Color.fromARGB(255, 0, 0, 0);
+const Color blueColor = Color(0xFF167AFA);
--- lib/navigations/navbar/navbar.dart
+++ lib/navigations/navbar/navbar.dart
@@ -1,3 +1,4 @@
+import 'package:cms/datatypes/datatypes.dart';
 import 'package:flutter/material.dart';
 
 class Navbar extends StatelessWidget {
@@ -5,6 +6,82 @@ class Navbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: blueColor),
+            child: GestureDetector(
+              onTap: () => Navigator.of(context).pop(),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                    backgroundImage: const AssetImage(
+                      "assets/images/img1profile.jpg",
+                    ),
+                    onBackgroundImageError: (exception, stackTrace) {},
+                    // child: const Icon(Icons.person, size: 35),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Amit Mahato", style: TextStyle(fontSize: 20)),
+                        Text("Admin", style: TextStyle(fontSize: 15)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text("Profile"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ExpansionTile(
+            leading: Icon(Icons.people),
+            title: Text("Staff"),
+            children: [
+              ListTile(
+                leading: Icon(Icons.person_4),
+                title: Text(" Teaching"),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: Icon(Icons.engineering),
+                title: Text("Non-Teaching"),
+              ),
+            ],
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: const Text("Send Notification"),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.policy_rounded),
+            title: Text("Policies"),
+            onTap: () => Navigator.pop(context),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("Logout"),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
   }
 }
